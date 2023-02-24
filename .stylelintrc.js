5decb081282bf2d72b8117db2948f9e2413ac87c
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-02-24 17:17:01 +0330

    feat: add dx dependencies

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-sass-guidelines",
+        "stylelint-prettier/recommended",
+    ],
+    rules: {
+        "selector-class-pattern": null,
+    },
+};
